1732818dd82bfc996ef28126264c12cb4f28e181
Alien <alongejoshua@gmail.com>
2023-02-03 19:08:20 +0100

    defines and annotate multiple variables with specified values

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" define and annotate multiple variables with specified values """
+
+a: int = 1 # defines a as int
+pi: float = 3.14 # defines pi as float
+i_understand_annotations: bool = True # defines i_understand_annotations as bool
+school: str = "Holberton" # defines school as str
