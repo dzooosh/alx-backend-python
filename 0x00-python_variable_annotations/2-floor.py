c027b598ea3ca2a1979d214f9cf6f07912883c03
Alien <alongejoshua@gmail.com>
2023-02-03 18:55:39 +0100

    returns the type-annotated argument

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" type-annotated function floor
+    args: (float) n
+    returns: floor of the float """
+def floor(n: float) -> int:
+    """ returns the floor of the float argument """
+    return int(n)
