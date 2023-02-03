5ff4ad1a19fbba17e7d5295e6c4ce61154f4cad8
Alien <alongejoshua@gmail.com>
2023-02-03 17:40:35 +0100

    type-annotated function concat that takes two strings and return a concat.. string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" type annotated function concat
+    arg1: str1
+    arg2: str2
+    return the concatenated string
+"""
+def concat(str1: str, str2: str) -> str:
+    """ concatenate two strings """
+    return (str1 + str2)
