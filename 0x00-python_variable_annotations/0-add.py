46fb3f84178507fa12945eaaa74a7b2ad5e17245
Alien <alongejoshua@gmail.com>
2023-02-03 17:13:48 +0100

    type annotated function that returns sum as a float

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python3
+''' Type annotated function that takes in float arguments
+    returns float result'''
 def add(a: float, b: float) -> float:
+    """ adds two args and return the result """
     return a + b
