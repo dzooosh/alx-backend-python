195242cc028433ddb79b237b5dcf906fb3b1db00
Alien <alongejoshua@gmail.com>
2023-04-30 23:49:21 +0100

    adjusts the annotation returned

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,11 @@
 """ Asynchronous Generator """
 import random
 import asyncio
-from typing import List
+import typing
 
-
-async def async_generator() -> List[float]:
+async def async_generator() -> typing.Generator[float,
+                                                None,
+                                                None]:
     """
     Async_generator - generates random number between
     0 and 10 using coroutine to loop 10 times each time
