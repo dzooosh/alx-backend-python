96cad2736521fb8df10ba8cd09f818ac54b8ca56
Alien <alongejoshua@gmail.com>
2023-04-30 23:42:16 +0100

    returns random numbers from 0 to 10 asynchronously when iterated

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+""" Asynchronous Generator """
+import random
+import asyncio
+from typing import List
+
+
+async def async_generator() -> List[float]:
+    """
+    Async_generator - generates random number between
+    0 and 10 using coroutine to loop 10 times each time
+    asynchronously
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        # yield returns value from the coroutime without termination
+        yield random.uniform(0, 10)
